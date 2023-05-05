521a5cabed73a8cda284a883494d4cd6d2e5522c
ljm07122 <132687668+ljm07122@users.noreply.github.com>
2023-05-06 08:57:44 +0900

    Update 銀行検索.js

--- 銀行検索.js
+++ 銀行検索.js
@@ -4,21 +4,21 @@ var reader = new FileReader();
 var table = document.getElementById('table');
 var search = document.getElementById('search');
 var searchbtn = document.getElementById('searchbtn');
-var ddl = document.getElementById('ddl')
+var ddl = document.getElementById('ddl');
+let message = document. getElementById('message');
 
 
-
-// csvファイル読み込み時の動作
-upload.addEventListener('change', function () {
-    var file = upload.files[0];
-    if (!file) alert('ファイルを選択してください。');
-    reader.readAsText(file, 'shift-jis');
-
-  //銀行の選択肢の生成 
-    reader.onload = function () {
-        let tableresult = reader.result.split('\r\n');
-        let td = reader.result.split(/\r\n|,/);
-        for(let i=0;i<=tableresult.length; i++){if(td[i*5+4] == 1){ddl.innerHTML += `<option value= "${td[i*5]}"> ${td[i*5+3]}</option>`}}}
+windows.onload = function(){
+  var req = new XMLHttpRequest();
+  req.open("get","ginkositen.csv",true);
+  req.send(null);
+  req.onload = function(){
+      req.readAsText(file,'shift-jis');
+      var tableresult = str.split('\r\n'); 
+      var td = str(/\r\n|,/);
+        for(let i=0;i<=tableresult.length; i++){
+          let as = td[i*5+3].replaceAll('"','')
+          if(td[i*5+4] == 1){ddl.innerHTML += `<option value= "${td[i*5]}"> ${as}</option>`};};
 
 
 
@@ -29,10 +29,12 @@ ddl.addEventListener('change',function(){
         let td = reader.result.split(/\r\n|,/);
         table.innerHTML = `<tr style="background-color: yellow;"><th class="gincode">金融機関コード</th><th class="sitencode">支店コード</th><th class="sitennamekana">支店名カナ</th><th class="sitenname">支店名</th></tr>`
          for(let i = 0; i <= tableresult.length; i++){
+            let ab = td[i*5+2].replaceAll('"','')
+            let as = td[i*5+3].replaceAll('"','')
             if (tableresult[i] === undefined){ none = "none"}
             else{none = "null"}
             if(td[i*5] == ddl.value){if(td[i*5+4] == 2){
-         table.innerHTML += `<tr style="display:${none}; text-align: center;"><td>${td[i*5]}</td><td>${td[i*5+1]}</td><td>${td[i*5+2]}</td><td>${td[i*5+3]}</td></tr>`;}    }}})
+              table.innerHTML += `<tr style="display:${none}; text-align: center;"><td>${td[i*5]}</td><td>${td[i*5+1]}</td><td>${ab}</td><td>${as}</td></tr>`;}    }}})
 
          
 
@@ -43,6 +45,12 @@ searchbtn.addEventListener('click',function(){
       table.innerHTML = `<tr style="background-color: yellow;"><th class="gincode">金融機関コード</th><th class="sitencode">支店コード</th><th class="sitennamekana">支店名カナ</th><th class="sitenname">支店名</th></tr>`
 
             for(let i = 0; i <= tableresult.length; i++){
+              let ab = td[i*5+2].replaceAll('"','')
+              let as = td[i*5+3].replaceAll('"','')
               if(td[i*5] == ddl.value){
-              if (td[i*5+3].indexOf(search.value) !== -1){ table.innerHTML += `<tr style="text-align: center"><td>${td[i*5]}</td><td>${td[i*5+1]}</td><td>${td[i*5+2]}</td><td>${td[i*5+3]}</td></tr>`;}
-          }}})})
+              if (td[i*5+3].indexOf(search.value) !== -1){
+                table.innerHTML += `<tr style="text-align: center"><td>${td[i*5]}</td><td>${td[i*5+1]}</td><td>${ab}</td><td>${as}</td></tr>`;}
+                else{if(td[i*5+1].indexOf(search.value) !== -1){
+                table.innerHTML += `<tr style="text-align: center"><td>${td[i*5]}</td><td>${td[i*5+1]}</td><td>${ab}</td><td>${as}</td></tr>`;}
+              }
+          }}})}}
