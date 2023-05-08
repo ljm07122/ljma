bf7b8649e41c3378a2f7e216416e5a2219805c9e
ljm07122 <132687668+ljm07122@users.noreply.github.com>
2023-05-08 19:26:05 +0900

    Update 銀行検索.js

--- 銀行検索.js
+++ 銀行検索.js
@@ -6,6 +6,8 @@ var ddl = document.getElementById('ddl');
 var ddl1 = document.getElementById('ddl1')
 var message = document. getElementById('message');
 
+
+
 // サーバー上のファイル読み取り
 var request = new XMLHttpRequest(); 
 csv_data('https://ljm07122.github.io/ljma/ginkositen.csv');
@@ -14,10 +16,8 @@ function csv_data(dataPath) {
 		    const response = request.responseText; 
         let tableresult = response.split('\r\n');
         let td = response.split(/\r\n|,/);
-
         for(let i=0;i<=tableresult.length; i++){
-           let as = td[i*5+3].replaceAll('"','')
-        
+           let as = td[i*5+3].replaceAll('"','')        
            if(td[i*5+4] == 1){ddl1.innerHTML += `<option value= "${td[i*5]}" > ${as}</option>`};message.innerHTML = `読み取り完了`;}});
         request.open('GET', dataPath, true);
         request.send();}
@@ -29,27 +29,33 @@ ddl.addEventListener('change',function(){
         let tableresult = response.split('\r\n');
         let td = response.split(/\r\n|,/)
         table.innerHTML = `<tr style="background-color: yellow;"><th class="gincode">金融機関コード</th><th class="sitencode">支店コード</th><th class="sitennamekana">支店名カナ</th><th class="sitenname">支店名</th></tr>`
+        console.log(search.value)
         for(let i = 0; i <= tableresult.length; i++){
-           let ab = td[i*5+2].replaceAll('"','')
-           let as = td[i*5+3].replaceAll('"','')
-           if (tableresult[i] === undefined){ none = "none"}
-           else{none = "null"}
-           if(td[i*5] == ddl.value){if(td[i*5+4] == 2){
+          let ab = td[i*5+2].replaceAll('"','')
+          let as = td[i*5+3].replaceAll('"','')
+          if (tableresult[i] === undefined){ none = "none"}
+             else{none = "null"}
+          if(td[i*5] == ddl.value){if(td[i*5+4] == 2){
              table.innerHTML += `<tr style="display:${none}; text-align: center;"><td>${td[i*5]}</td><td>${td[i*5+1]}</td><td>${ab}</td><td>${as}</td></tr>`;}}}})
 
          
 
-// 支店名（漢字）検索時の動作
+// 支店名検索時の動作
 searchbtn.addEventListener('click',function(){
         const response = request.responseText;
         let tableresult = response.split('\r\n');
         let td = response.split(/\r\n|,/);
-        table.innerHTML = `<tr style="background-color: yellow;"><th class="gincode">金融機関コード</th><th class="sitencode">支店コード</th><th class="sitennamekana">支店名カナ</th><th class="sitenname">支店名</th></tr>`
-        for(let i = 0; i <= tableresult.length; i++){
-           let ab = td[i*5+2].replaceAll('"','')
-           let as = td[i*5+3].replaceAll('"','')
-           if(td[i*5] == ddl.value){
-              if (td[i*5+3].indexOf(search.value) !== -1){
-                table.innerHTML += `<tr style="text-align: center"><td>${td[i*5]}</td><td>${td[i*5+1]}</td><td>${ab}</td><td>${as}</td></tr>`;}
-                else{if(td[i*5+1].indexOf(search.value) !== -1){
-                table.innerHTML += `<tr style="text-align: center"><td>${td[i*5]}</td><td>${td[i*5+1]}</td><td>${ab}</td><td>${as}</td></tr>`;}}}}})
+        if(search.value == ""){alert('検索キーワードを設定してください')}else{
+          table.innerHTML = `<tr style="background-color: yellow;"><th class="gincode">金融機関コード</th><th class="sitencode">支店コード</th><th class="sitennamekana">支店名カナ</th><th class="sitenname">支店名</th></tr>`
+          for(let i = 0; i <= tableresult.length; i++){
+             let ab = td[i*5+2].replaceAll('"','')
+             let as = td[i*5+3].replaceAll('"','')
+             if(ddl.value == ""){
+                if(td[i*5+3].indexOf(search.value) !== -1){table.innerHTML += `<tr style="text-align: center"><td>${td[i*5]}</td><td>${td[i*5+1]}</td><td>${ab}</td><td>${as}</td></tr>`;}
+                   else{if(td[i*5+1].indexOf(search.value) !== -1){
+                      table.innerHTML += `<tr style="text-align: center"><td>${td[i*5]}</td><td>${td[i*5+1]}</td><td>${ab}</td><td>${as}</td></tr>`;}}}
+                else{if(td[i*5] == ddl.value){
+                   if (td[i*5+3].indexOf(search.value) !== -1){
+                      table.innerHTML += `<tr style="text-align: center"><td>${td[i*5]}</td><td>${td[i*5+1]}</td><td>${ab}</td><td>${as}</td></tr>`;}
+                      else{if(td[i*5+1].indexOf(search.value) !== -1){
+                         table.innerHTML += `<tr style="text-align: center"><td>${td[i*5]}</td><td>${td[i*5+1]}</td><td>${ab}</td><td>${as}</td></tr>`;}}}}}}})
